e0afca2aa51d688d12872fc1b722155fbe443c42
Shubham Gaud <shubhamgaud18@gmail.com>
2022-07-13 13:10:01 +0530

    Added Test in dao

---
+++ StudentInformationSystem/src/dao/Test.java
@@ -0,0 +1,20 @@
+package dao;
+
+public class Test {
+	
+	String name;
+	String addrss;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getAddrss() {
+		return addrss;
+	}
+	public void setAddrss(String addrss) {
+		this.addrss = addrss;
+	}
+
+}
