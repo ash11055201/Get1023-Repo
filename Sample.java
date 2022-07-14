4cfef6c1d2d5ae7e6bc4315563135903106ac66d
ash11055201 <42478515+ash11055201@users.noreply.github.com>
2022-07-14 17:21:25 +0530

    Update Sample.java

--- Sample.java
+++ Sample.java
@@ -2,5 +2,6 @@ class Sample{
   public static void main(String[] args){
     System.out.println("Hello WOrld");
     System.out.println("Hello Vikrant");
+    System.out.println("Hello Hello");
   }
 }
